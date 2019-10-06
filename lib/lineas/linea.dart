4665c4829f2afd823df36ac0fb079f2efeab83c7
Juan Cruz <Mare>
2019-10-06 00:01:42 -0300

    differentiated lines with colors

--- lib/lineas/linea.dart
+++ lib/lineas/linea.dart
@@ -17,14 +17,24 @@ void getData(linea) async {
   Response response=await dio.get("https://200.123.180.122:5743/rest/posicionesBuses/"+linea);  //CERTIFICATE_VERIFY_FAILED:ok
   buses = response.data['posiciones'];
   markers.clear();
-  buses.forEach((value){
-    var marker = Marker(markerId: MarkerId(value['interno']),
-      draggable: false,
-      icon: BitmapDescriptor.fromAsset('assets/bus.png',),
-      position: LatLng(value["latitud"], value["longitud"]),
-      rotation: value["orientacion"],
-      infoWindow: InfoWindow(title: "Interno" , snippet: value["interno"]),
-    );
-    markers.add(marker);
-  });
-}
+    buses.forEach((value){
+      if (int.parse(linea) >= 10 && int.parse(linea) <= 19 || int.parse(linea) >= 60 && int.parse(linea) <= 68){
+      var marker = Marker(markerId: MarkerId(value['interno']),
+        draggable: false,
+        icon: BitmapDescriptor.fromAsset('assets/coniferal.png',),
+        position: LatLng(value["latitud"], value["longitud"]),
+        rotation: value["orientacion"],
+        infoWindow: InfoWindow(title: "Interno" , snippet: value["interno"]),
+      );
+      markers.add(marker);
+      }else{
+        var marker = Marker(markerId: MarkerId(value['interno']),
+          draggable: false,
+          icon: BitmapDescriptor.fromAsset('assets/ersa.png',),
+          position: LatLng(value["latitud"], value["longitud"]),
+          rotation: value["orientacion"],
+          infoWindow: InfoWindow(title: "Interno" , snippet: value["interno"]),
+        );
+        markers.add(marker);
+      }
+    });}
